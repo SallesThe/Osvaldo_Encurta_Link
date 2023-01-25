550d19788788d489e25413466505f3a15e90e18c
SallesThe <manodada26@gmail.com>
2023-01-25 19:31:09 -0300

    Tentando criar conexão com API

--- assets/js/authentication.js
+++ assets/js/authentication.js
@@ -1,14 +1,35 @@
-// Token Acess: dea101ea0e249ef000c9b0e1a6dc1d05f563c26f
-
+const shortForm = document.querySelector(".main_content_form");
+const shortUrl = document.querySelector(".display-url");
+const copyUrl = document.querySelector(".copy_btn");
 const token = "dea101ea0e249ef000c9b0e1a6dc1d05f563c26f";
 
-// var fetch = require('node-fetch');
+shortForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-fetch('https://api-ssl.bitly.com/v4/shorten', {
-    method: 'POST',
-    headers: {
-        'Authorization': 'Bearer {token}',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ "long_url": "https://dev.bitly.com", "domain": "bit.ly", "group_guid": "Ba1bc23dE4F" })
+    const longUrl = document.querySelector("#long_url");
+    //const checkUrl = checkUrl();
+
+    const config = {
+        method: "POST",
+        headers: {
+            accept: "application/json", 
+            "content-type": "application/json",
+            api_key: "dea101ea0e249ef000c9b0e1a6dc1d05f563c26f"
+        },
+        body: JSON.stringify({destination: longUrl.value})
+    };
+
+    fetch("https://api-ssl.bitly.com/v4/shorten", config)
+        .then((response) => response.json())
+        .then((response) => {
+            shortUrl.innerHTML = response.shortUrl;
+            copyUrl.disable = "false"; 
+        })
+        .catch((err) => console.error(err));
 });
+ 
+copyUrl.addEventListener("click", () => {
+    simpleCopy(shortUrl.innerHTML);
+    alert("Copied");
+    copyUrl.disable = "true";
+})
