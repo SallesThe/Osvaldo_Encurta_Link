311ec7913a21a98c0236a1e7b41a27b9023a569a
Giulliano Guimarães <llthguimaraes@gmail.com>
2023-01-27 09:10:47 -0300

    Tentativa de API - 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,4 @@ fetch('https://api-ssl.bitly.com/v4/shorten?client_id=YOUR_CLIENT_ID&client_secr
     },
     body: JSON.stringify({ "long_url": "https://dev.bitly.com", "domain": "bit.ly", "group_guid": "Ba1bc23dE4F" })
 }).then((response) => response.json())
-.then((data) => console.log(data));
+    .then((data) => console.log(data));
